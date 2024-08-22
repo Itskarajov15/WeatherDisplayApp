13f5f105c1a4e45667fb9d3f7764f5558b56cb6b
Asen Karadzhov <asen__7@abv.bg>
2024-08-22 20:48:25 +0300

    Add last cities serched suggestions.

--- WeatherDisplayApp/weatherdisplayapp.ui/src/App.vue
+++ WeatherDisplayApp/weatherdisplayapp.ui/src/App.vue
@@ -2,7 +2,10 @@
   <div class="h-full w-full p-10 bg-[#111b2c]">
     <div class="w-1/2 mx-auto">
       <form class="flex gap-2 align-center justify-center" @submit.prevent="onSearch">
-        <input type="text" v-model="cityName" value="Sofia" class="p-2 bg-[#202B3D] text-white rounded-md border-none outline-none">
+        <input type="text" list="search" v-model="cityName" value="Sofia" class="p-2 bg-[#202B3D] text-white rounded-md border-none outline-none">
+        <datalist id="search">
+          <option v-for="city in cities" :key="city">{{city}}</option>
+        </datalist>
         <button class="bg-transparent text-white outline-none border-none" type="submit">Search</button>
       </form>
       <TheLoader v-if="showLoading" />
@@ -24,6 +27,7 @@ const cityName = ref('');
 const weather = ref(null);
 const errorMessage = ref(null);
 const showLoading = ref(false);
+const cities = ref(JSON.parse(localStorage.getItem("cities") || '[]'));
 
 async function onSearch() {  
   errorMessage.value = null;
@@ -33,6 +37,8 @@ async function onSearch() {
     const data = await getWeather(cityName.value);
 
     weather.value = data;
+    cities.value = [...new Set([cityName.value, ...cities.value])];
+    localStorage.setItem("cities", JSON.stringify(cities.value));
   } catch (err) {
     errorMessage.value = err.message;
   }
